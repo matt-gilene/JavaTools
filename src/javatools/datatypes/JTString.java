31bae6b0855d8c7d8aedfdb5068dc560631f1456
Matt Gilene <mdgilene@yahoo.com>
2014-06-24 22:19:09 -0400

    Added 2 starter classes

---
+++ src/javatools/datatypes/JTString.java
@@ -0,0 +1,7 @@
+package javatools.datatypes;
+
+/**
+ * Created by Matt on 6/24/2014.
+ */
+public class JTString {
+}
---
+++ src/javatools/fileio/JTOut.java
@@ -0,0 +1,7 @@
+package javatools.fileio;
+
+/**
+ * Created by Matt on 6/24/2014.
+ */
+public class JTOut {
+}
